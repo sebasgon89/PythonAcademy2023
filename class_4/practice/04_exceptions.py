95a59779bc1d423c8ab5b085e79664c5b1a785dd
sebasgon89 <seba3789@gmail.com>
2023-04-27 10:10:00 -0300

    Added exam

--- class_4/practice/04_exceptions.py
+++ class_4/practice/04_exceptions.py
@@ -1,11 +1,16 @@
 # 1
 # create a piece of code to trap an IndexError Exception
-
+a = ["a","b","c"]
+try:
+    print(a[4])
+e
 # 2
 # create a piece of code to trap an ArithmeticError Exception
 
 # 3
 # create a piece of code to trap an TypeError Exception
+a = ["a","b","c"]
+a(4)
 
 # 4
 # create a piece of code to trap an NameError Exception
